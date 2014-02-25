b78d44ea147b4e0617eab65f063a3b28d4294c05
Animal <anima_black@mail.ru>
2014-02-25 13:04:17 +0200

    Crashlytics changes. Camfind -> Impctful

--- CamFind/CFAppDelegate.m
+++ CamFind/CFAppDelegate.m
@@ -16,9 +16,9 @@
 @implementation CFAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-//    [Crashlytics startWithAPIKey:@"ed60b7f019cab46431bad5d3b5a09e299f1fab0c"];
-    [Crashlytics startWithAPIKey:@"c4dcde3d75563274731fe0519d4220e84b151323"];
-
+//    [Crashlytics startWithAPIKey:@"c4dcde3d75563274731fe0519d4220e84b151323"];
+    [Crashlytics startWithAPIKey:@"ed60b7f019cab46431bad5d3b5a09e299f1fab0c"];
+    
     self.tagManager = [TAGManager instance];
     [self.tagManager.logger setLogLevel:kTAGLoggerLogLevelDebug];
     [TAGContainerOpener openContainerWithId:@"GTM-TS8WFN"   // own
